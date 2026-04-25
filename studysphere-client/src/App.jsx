e716df3b292bd24cf81c03120dfd099fdb221fd0
aishat aminu <aishat.aminu@Student.griffith.ie>
2026-04-25 18:45:50 +0100

    Saving some frontend work before checking Aisha branch

--- studysphere-client/src/App.jsx
+++ studysphere-client/src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./styles/Layout.css";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
