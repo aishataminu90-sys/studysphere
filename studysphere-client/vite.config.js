572c0e43db6f0ef4f0b9560b06680ac2f604606f
Michelle <meotomewo4@gmail.com>
2026-05-02 23:38:41 +0100

    Update vite config for Render deployment

--- studysphere-client/vite.config.js
+++ studysphere-client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], envPrefix: ['VITE_','REACT_APP_']
+  plugins: [react()], envPrefix: ['VITE_','REACT_APP_'],
+
+  preview: {
+    port: 4173,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
